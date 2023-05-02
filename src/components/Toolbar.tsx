0dfb9c664b9ffbf1fedd69dfd8e9037e25c7cb4e
RubenHenares <rhenares@uxland.cat>
2023-05-02 21:43:02 +0200

    mejoraToolbar

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -5,14 +5,14 @@ import { routes } from '../router'
 export const Toolbar= ()=>{
   return (
     <div className='text-end sm:text-xl text-blue-900 font-semibold'>
-      <div className=''>
-        <Link to={routes.home} className="mr-3"> <button className='hover:scale-x-105 hover:scale-y-105 transition duration-150'>Home</button></Link>
-        <Link to={routes.profesiones} className="mr-3"><button className='hover:scale-x-105 hover:scale-y-105 transition duration-150'>Profesiones</button></Link>
-        <Link to={routes.pacientes} className="mr-3"> <button className='hover:scale-x-105 hover:scale-y-105 transition duration-150'>Pacientes</button></Link>
-        <Link to={routes.somos} className="mr-3"><button className='hover:scale-x-105 hover:scale-y-105 transition duration-150'>Quienes somos</button></Link>
+      <div className='flex-justify-end'>
+        <Link to={routes.home} className="mr-3"> <button className='hover:text-indigo-500 hover:scale-x-105 hover:scale-y-105 transition duration-150'>Home</button></Link>
+        <Link to={routes.profesiones} className="mr-3"><button className='hover:text-indigo-500 hover:scale-x-105 hover:scale-y-105 transition duration-150'>Profesiones</button></Link>
+        <Link to={routes.pacientes} className="mr-3"> <button className='hover:text-indigo-500 hover:scale-x-105 hover:scale-y-105 transition duration-150'>Pacientes</button></Link>
+        <Link to={routes.somos} className="mr-3"><button className='hover:text-indigo-500 hover:scale-x-105 hover:scale-y-105 transition duration-150'>Quienes somos</button></Link>
       </div>
-      <div className=" dark:bg-slate-800 rounded-lg px-6 py-8">
-        <span className="inline-flex items-center justify-center p-2 bg-indigo-500 rounded-md shadow-lg">
+      <div className="flex dark:bg-slate-800 rounded-lg px-6 py-8">
+        <span className=" p-2 bg-indigo-500 rounded-md shadow-lg">
           <svg className="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"></svg>
         </span>
       </div>
